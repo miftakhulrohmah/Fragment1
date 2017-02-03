39d59425eb18208f56f7334d97ddb1c2a077301d
Miftahul Rohmah <miftahul_rohmah_24rpl@student.smktelkom-mlg.sch.id>
2017-02-03 09:43:01 +0700

    Fragment Independent View and Process

--- app/src/main/java/id/sch/smktelkom_mlg/learn/fragment1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/fragment1/MainActivity.java
@@ -2,33 +2,19 @@ package id.sch.smktelkom_mlg.learn.fragment1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    EditText etNama;
-    EditText etUmur;
-    TextView tvHasil;
-
+public class MainActivity extends AppCompatActivity
+  implements InputFragment.IListener
+{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+   }
 
-        etNama = (EditText) findViewById(R.id.editTextNama);
-        etUmur = (EditText) findViewById(R.id.editTextUmur);
-        tvHasil = (TextView) findViewById(R.id.textViewHasil);
-
-        findViewById(R.id.buttonProses).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                doProsess();
-            }
-        });
-    }
-
-    private void doProsess() {
-        tvHasil.setText(etNama.getText().toString() +" ("+etUmur.getText().toString()+" tahun)");
+    @Override
+    public void doProsess(String nama, String umur) {
+        OutputFragment fragOut = (OutputFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentOutput);
+        fragOut.doProsess(nama, umur);
     }
 }
